e4794b116d6e52cca5858ea4c8427d096772385a
Tim Hart <thart@apple.com>
2022-10-13 13:50:58 -0400

    Super big update

--- Button Tapper/ViewController.swift
+++ Button Tapper/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.systemPurple
     }
 
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        count = count + 1
+        theLabel.text = "\(count)"
+    }
 }
 
